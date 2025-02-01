eca941041a55b396209da884680906255b12662b
Gert-Jan Paulissen <12508981+gpaulissen@users.noreply.github.com>
2025-02-01 18:32:41 +0100

    Run either django runserver or uswgi.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-set -e
+set -ue
+set -x
 
-ls -la /vol/
-ls -la /vol/web
+basedir=$(cd $(dirname $0) && pwd)/..
+ls -la ${STATIC:-$basedir/data/web/static}
+ls -la ${MEDIA:-$basedir/data/web/media}
 
 whoami
 
@@ -13,7 +15,7 @@ python manage.py migrate
 
 case "${DEBUG:-0}" in
     0)
-        uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
+        uwsgi --http 9000 --workers 4 --master --enable-threads --module app.wsgi
         ;;
     *)
         python manage.py runserver 8000
