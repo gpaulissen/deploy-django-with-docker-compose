eed5335adc618d6ddc940662d473bc2e33b0e80c
Gert-Jan Paulissen <12508981+gpaulissen@users.noreply.github.com>
2025-02-02 10:51:53 +0100

    Adding gunicorn.

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,11 +13,21 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-case "${DEBUG:-0}" in
-    0)
+export DEBUG=1
+
+case "${APP_SERVER:-}" in
+    gunicorn)
+        gunicorn \
+            --bind=unix:$NGINX_TMPDIR/gunicorn.sock \
+            --workers=${WORKERS:-4} \
+            --threads=${THREADS:-2} \
+            app.wsgi
+        ;;
+    uwsgi)
         uwsgi --socket :8000 --workers 4 --master --enable-threads --module app.wsgi
         ;;
     *)
+        DEBUG=0
         python manage.py runserver 8000
         ;;
 esac
